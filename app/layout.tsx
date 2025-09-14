660d268557dd3cf0125e0abc70e7c746d4508b47
Bruno Penzar <penzar.bruno@gmail.com>
2025-09-15 01:18:25 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";              // <= DODAJ OVO
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { ThemeProvider } from "./components/theme-switch";
 import { metaData } from "./lib/config";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
