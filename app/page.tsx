d05d52126842a0d038a53cc39d976b53d64270af
Bruno Penzar <penzar.bruno@gmail.com>
2025-09-15 02:40:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,10 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-medium">Portfolio template!</h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS.
+         
         </p>
         <p>
-          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
-          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
+          {""}
           <a
             target="_blank"
             href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
@@ -33,11 +31,11 @@ export default function Page() {
           .
         </p>
         <p>
-          Nextfolio is{" "}
+          {" "}
           <a href={socialLinks.github} target="_blank">
             open-source
           </a>{" "}
-          and fully customizable, making it easy to add more features.
+         
         </p>
         <p>
           <a
@@ -46,8 +44,7 @@ export default function Page() {
           >
             Deploy
           </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
+         {" "}
           <a href="/blog/getting-started">Getting Started</a> post.
         </p>
         <p>
