c574b3c3e65c1cc79b5547e29afabbc37265a6c2
Bruno Penzar <penzar.bruno@gmail.com>
2025-09-25 13:55:49 +0200

    design

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,20 +9,20 @@ export const metadata: Metadata = {
 
 export default function Projects() {
   return (
-    <section className="min-h-full flex items-center">
-      <div className="mx-auto w-full max-w-6xl 2xl:max-w-7xl px-6 flex flex-col items-center space-y-30">
+    <section className="py-8 md:py-12">
+      <div className="mx-auto w-full max-w-6xl 2xl:max-w-7xl px-6 flex flex-col items-center space-y-8 md:space-y-14">
         {/* Naslov i podnaslov */}
         <div className="text-center">
-          <h1 className="text-4xl md:text-5xl font-semibold tracking-tight">
+          <h1 className="text-3xl md:text-5xl font-semibold tracking-tight">
             Projekte
           </h1>
-          <p className="mt-3 text-sm md:text-base text-neutral-600 dark:text-neutral-400">
+          <p className="mt-2 md:mt-3 text-sm md:text-base text-neutral-600 dark:text-neutral-400">
             Izbor aktualnih projekata i alata u izradi.
           </p>
         </div>
 
         {/* Lista projekata */}
-        <div className="w-full space-y-12">
+        <div className="w-full space-y-8 md:space-y-12">
           {projects.map((project, i) => (
             <Link
               key={i}
@@ -32,10 +32,10 @@ export default function Projects() {
               className="group flex flex-col sm:flex-row sm:items-center sm:justify-between 
                          border-b border-neutral-200/20 dark:border-neutral-800/60 pb-3 hover:opacity-80"
             >
-              <h2 className="text-xl font-medium group-hover:underline">
+              <h2 className="text-lg md:text-xl font-medium group-hover:underline">
                 {project.title}
               </h2>
-              <p className="text-sm sm:text-base text-right text-black dark:text-white">
+              <p className="text-sm md:text-base text-right text-black dark:text-white">
                 {project.description}
               </p>
             </Link>
